06ffdd7938635ccb043207b3df99bdce7e9de025
Doppais <fefealvesdejesus@gmail.com>
2024-03-31 20:22:27 -0300

    mo preguiça ;-;

--- projeto/Tela.java
+++ projeto/Tela.java
@@ -1,12 +1,16 @@
 package projeto;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.FileNotFoundException;
 
-public class Tela extends JFrame {
+public class Tela extends JFrame implements KeyListener {
 	int x_size=1600,y_size=800;
 	int borda=8,topo_borda=32;
 	int atual_monstro=0;
 	int espaco=300;
+	private Jogo jogo;
 	private boolean monstro=true;
     private Image sprite;
     private Image map;
@@ -14,12 +18,12 @@ public class Tela extends JFrame {
     Image[] monstros = new Image[4];
     
     
-    public Tela() {
+    public Tela() throws FileNotFoundException {
         setTitle("Meu Jogo");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(x_size,y_size);
         setLocationRelativeTo(null);
-        
+        this.jogo=new Jogo("\\Dados.csv");
         ImageIcon icon = new ImageIcon("C:\\Users\\doppa\\OneDrive\\Área de Trabalho\\Nova pasta\\img\\nave_azul.png");
         sprite = icon.getImage();
         icon = new ImageIcon("C:\\Users\\doppa\\OneDrive\\Área de Trabalho\\map.jpg");
@@ -34,6 +38,8 @@ public class Tela extends JFrame {
         
  
         monstros[0] = sprite;
+        this.setVisible(true);
+        this.repaint();
     }
 
     @Override
@@ -62,17 +68,34 @@ public class Tela extends JFrame {
         g.drawString(currentLine, x, y);
     }
     
-    public void jogar() {
-    	Jogo jogo=new Jogo("o arquivo la do grafo");
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Este método é chamado quando uma tecla é digitada (pressionada e solta)
+    	char tecla=e.getKeyChar();
+    	switch (tecla) {
+    		case 'a':
+    		case 'A':
+        	 
+    			break;
+    		default:
+             //nada
+     }
     	
+    	
+    	
+    	this.repaint();
+    }
+    
+    public void jogar() {
+    	  	
     }
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             Tela jogo = new Tela();
             jogo.setVisible(true);
             jogo.repaint(); // Certifique-se de que a tela seja atualizada
         });
-    }
+    }*/
 }
 
